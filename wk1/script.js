58dd029c72bef7ce74418c74fe27cd59871a7162
Kallie Rollenhagen <krollenhagen@mcad.edu>
2024-01-16 21:19:59 -0600

    first commit

---
+++ wk1/script.js
@@ -0,0 +1,6 @@
+// what is a variable?
+
+var myVariable = "bob";
+let myNumber = 10;
+console.log("myVariable is " + myVariable); //plus sign (concatenation operator)
+console.log("mynumber " + myNumber)
