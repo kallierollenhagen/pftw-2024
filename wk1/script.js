774bffd9783f1787f976966ce1bbd7b8e5db94c4
Kallie Rollenhagen <krollenhagen@mcad.edu>
2024-01-17 20:55:29 -0600

    committing changes

--- wk1/script.js
+++
@@ -1,6 +0,0 @@
-// what is a variable?
-
-var myVariable = "bob";
-let myNumber = 10;
-console.log("myVariable is " + myVariable); //plus sign (concatenation operator)
-console.log("mynumber " + myNumber)
