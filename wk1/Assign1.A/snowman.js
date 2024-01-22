03ea32c3f2b78c06c30659e1e663550d17ea2271
Kallie Rollenhagen <krollenhagen@mcad.edu>
2024-01-21 19:57:11 -0600

    snowman changes

--- wk1/Assign1.A/snowman.js
+++ wk1/Assign1.A/snowman.js
@@ -1,39 +1,90 @@
 let grid = undefined;
-let stroke1 = prompt("Enter a basic color name in lower case", "gray");
-let stroke2 = prompt("Enter another basic color lowercase", "purple");
+//let stroke1 = prompt("Enter a basic color name in lower case", "gray");
+//let stroke2 = prompt("Enter another basic color lowercase", "purple");
+
+// SNOW = random dot generator by hafiyyandi
+
+var dAmt = 400;
 
 function setup() {
-    createCanvas(1000, 800);
-    background("#ccc");
-    grid = loadImage("images/100px_grid.png");
+  createCanvas(1000, 800);
+  background(0, 158, 255);
+  noStroke();
+  
+  fill('white');
+  for (var i = 0; i < dAmt; i++){
+    var al = random (30, 255);
+    var r_col = random (150,255);
+    var g_col = random (150, 255);
+  	var y = random(0, height);
+    var x = random(0, width);
+    var r = random(2, 10);
+    fill(249, 246, 238);
+    ellipse(x, y, r, r); 
 }
-function draw() {
-    background(grid);
+
+//White hill
+    fill('white');
+    quad(0, 770, 1000, 650, 1000, 800, 0, 800); 
+
+//SNOWMAN large
     // snowman legs
-    fill("#f1f1f1");
-    strokeWeight(20)
-    stroke(stroke1);
-    // left leg
-    ellipse(350, 650, 200);
-    //right leg
-    ellipse(650, 650, 200);
-    // body
-    ellipse(500, 450, 350, 400);
+    fill(250, 249, 246);
+    strokeWeight(10)
+    stroke(55, 204, 0); // replace with stroke(stroke1);
+    // body - bottom
+    ellipse(650, 600, 350, 350);
+    // body - middle
+    ellipse(650, 400, 250, 250);
     // head
-    ellipse(500, 200, 200);
+    ellipse(650, 200, 200);
+    // left arm
+    stroke(51, 25, 0);
+    strokeWeight(15);
+    line(400, 350, 525, 400);
+    //right arm
+    line(775, 400, 900, 350)
     // hat brim
-    stroke(stroke2);
-    strokeWeight(40);
-    line(400, 120, 600, 120);
+    stroke(20, 20, 60);  // replace with  stroke(stroke2);
+    strokeWeight(20);
+    line(550, 120, 750, 120);
     // hat body
-    quad(400, 50, 600, 50, 550, 120, 450, 120);
+    rect(575, 10, 145, 90);
     // eyes
     stroke(0);
-    strokeWeight(50);
-    point(425, 200);
-    point(575,200);
+    strokeWeight(40);
+    point(610, 185);
+    point(690, 185);
     // mouth
     noFill();
     strokeWeight(10);
-    arc(500, 200, 80, 80, 0, HALF_PI);
+    arc(650, 210, 100, 100, 120, 103);
+    //buttons
+    strokeWeight(30);
+    point(650, 360);
+    point(650, 400);
+    point(650, 440);
+
+
+//SNOWMAN small
+
+    fill(250, 249, 246);
+    strokeWeight(10)
+    stroke(55, 204, 0); // replace with stroke(stroke1);
+    // body - bottom
+    ellipse(250, 650, 250, 265);
+    // body - top
+    ellipse(250, 475, 140, 175);
+    // eyes
+    stroke(0);
+    strokeWeight(35);
+    point(220, 465);
+    point(280, 465);
+    // mouth
+    strokeWeight(30);
+    point(250, 515);
+    //buttons
+    strokeWeight(30);
+    point(250, 630);
+    point(250, 670);
 }
